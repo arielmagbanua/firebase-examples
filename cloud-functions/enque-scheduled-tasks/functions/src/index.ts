1c55438cd68a1d4e6458bc9dbc09798964408ee7
unknown <magbanua.c.ariel@gmail.com>
2022-06-21 20:43:22 +0800

    Added triggering of task by post request.

--- cloud-functions/enque-scheduled-tasks/functions/src/index.ts
+++ cloud-functions/enque-scheduled-tasks/functions/src/index.ts
@@ -1,12 +1,10 @@
 import * as express from 'express';
 import * as cors from 'cors';
 import * as functions from 'firebase-functions';
-import { getFunctions } from 'firebase-admin/functions';
 import * as admin from 'firebase-admin';
+import axios from 'axios';
 
 const serviceAccount = require('../keyfile.json');
-
-console.log(serviceAccount);
 
 // initialize Firebase
 const adminApp = admin.initializeApp({
@@ -22,7 +20,8 @@ const expressApp = express();
 // automatically allow cross-origin requests
 expressApp.use(cors({ origin: true }));
 
-const functionsRegion = functions.region('asia-east1');
+const location = 'asia-east1';
+const functionsRegion = functions.region(location);
 
 // create a document with tasks
 export const createDocumentTask = functionsRegion.tasks.taskQueue({
@@ -48,9 +47,6 @@ export const createDocumentTask = functionsRegion.tasks.taskQueue({
 
 expressApp.post('/createDocument', async (req, res) => {
   const { collection, data } = req.body;
-  
-  const queue = getFunctions().taskQueue('createDocumentTask');
-  console.log(queue);
 
   const dataPayload = {
     data: {
@@ -59,10 +55,22 @@ expressApp.post('/createDocument', async (req, res) => {
     },
   };
 
-  queue.enqueue(dataPayload, {
-    scheduleDelaySeconds: 5,
-    dispatchDeadlineSeconds: 60
-  });
+  const url = `https://asia-east1-fir-examples-1f2df.cloudfunctions.net/createDocumentTask`;
+  const result = await axios.post(url, dataPayload, {
+    headers: {
+      'Content-type': 'application/json',
+      'Authorization': `Bearer ${process.env.API_KEY}`
+    }
+  })
+    .then(function (response) {
+      return response;
+    })
+    .catch(function (error) {
+      console.log(error);
+      return null;
+    });
+
+  console.log(result);
 
   res.send({ success: true });
 });
@@ -71,28 +79,4 @@ expressApp.get('/test', async (req, res) => {
   res.send({ success: true });
 });
 
-export const testTask = functionsRegion.https.onRequest(async (req, res) => {
-  if (req.method == 'POST') {
-    const { collection, data } = req.body;
-
-    const dataPayload = {
-      data: {
-        collection: collection,
-        document: data,
-      },
-    };
-
-    console.log(dataPayload);
-
-    const queue = getFunctions().taskQueue('createDocumentTask');
-    await queue.enqueue(dataPayload);
-
-    res.send('Will create document');
-    return;
-  }
-
-  res.send({ success: true });
-});
-
 export const api = functionsRegion.https.onRequest(expressApp);
-
